955c519231a00083401760007cfe2f2c3b56f35c
Erma Safira Nurmasyita <erma.nurmasyita@tokopedia.com>
2023-08-13 19:22:44 +0700

    feat: register endpoints (dummy)

--- handler/endpoints.go
+++ handler/endpoints.go
@@ -1,18 +1,45 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/SawitProRecruitment/UserService/generated"
 	"github.com/labstack/echo/v4"
 )
 
-// This is just a test endpoint to get you started. Please delete this endpoint.
-// (GET /hello)
-func (s *Server) Hello(ctx echo.Context, params generated.HelloParams) error {
+func (s *Server) AddUser(ctx echo.Context) error {
+	var resp = generated.AddUserResponse{
+		Success: true,
+	}
+	return ctx.JSON(http.StatusOK, resp)
+}
 
-	var resp generated.HelloResponse
-	resp.Message = fmt.Sprintf("Hello User %d", params.Id)
+func (s *Server) LoginUser(ctx echo.Context) error {
+	token := "token-dummy"
+	userID := 1
+
+	var resp = generated.UserLoginResponse{
+		AuthToken: &token,
+		UserId:    &userID,
+	}
+	return ctx.JSON(http.StatusOK, resp)
+}
+
+func (s *Server) GetUser(ctx echo.Context) error {
+	var resp = generated.GetUserResponse{
+		Success: true,
+		Data: &generated.User{
+			FullName:    "Dummy",
+			PhoneNumber: "+62123",
+		},
+	}
+	return ctx.JSON(http.StatusOK, resp)
+}
+
+func (s *Server) UpdateUser(ctx echo.Context) error {
+	var resp = generated.UpdateUserResponse{
+		Success: true,
+		UserId:  nil,
+	}
 	return ctx.JSON(http.StatusOK, resp)
 }
--- handler/endpoints_test.go
+++ handler/endpoints_test.go
@@ -1,7 +1,226 @@
 package handler
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strings"
+	"testing"
 
-func TestHello(t *testing.T) {
+	"github.com/SawitProRecruitment/UserService/generated"
+	"github.com/bytedance/sonic"
+	"github.com/labstack/echo/v4"
+)
 
+func initHTTPCall(method, path string) (*httptest.ResponseRecorder, echo.Context) {
+	e := echo.New()
+	req := httptest.NewRequest(method, path, nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	return rec, c
+}
+
+func buildHTTPRequestBody(c echo.Context, method, path string, bodyRequest interface{}) echo.Context {
+	reqBytes, _ := sonic.Marshal(bodyRequest)
+	reqBody := string(reqBytes)
+	request, _ := http.NewRequest(method, path, strings.NewReader(reqBody))
+	request.Header.Add("Content-Type", "application/json")
+	c.SetRequest(request)
+
+	return c
+}
+
+func TestAddUser(t *testing.T) {
+	method, path := http.MethodPost, "/users"
+	rec, c := initHTTPCall(method, path)
+
+	type args struct {
+		ctx echo.Context
+		req generated.AddUserRequest
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantStatusCode int
+		wantRes        generated.AddUserResponse
+	}{
+		{
+			name: "success",
+			args: args{
+				req: generated.AddUserRequest{
+					FullName:    "full name",
+					Password:    "password",
+					PhoneNumber: "+621343",
+				},
+				ctx: func() echo.Context {
+					return c
+				}(),
+			},
+			wantStatusCode: http.StatusOK,
+			wantRes: generated.AddUserResponse{
+				Success: true,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Server{}
+
+			c = buildHTTPRequestBody(c, method, path, tt.args.req)
+			s.AddUser(c)
+
+			var got generated.AddUserResponse
+			sonic.Unmarshal(rec.Body.Bytes(), &got)
+
+			if rec.Code != tt.wantStatusCode {
+				t.Errorf("invalid status code, got: %v, want: %v", rec.Code, tt.wantStatusCode)
+			}
+
+			if !reflect.DeepEqual(got, tt.wantRes) {
+				t.Errorf("invalid body response, got: %v, want: %v", got, tt.wantRes)
+			}
+		})
+	}
+}
+
+func TestUpdateUser(t *testing.T) {
+	method, path := http.MethodPatch, "/users"
+	rec, c := initHTTPCall(method, path)
+
+	type args struct {
+		req generated.UpdateUserRequest
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantStatusCode int
+		wantRes        generated.UpdateUserResponse
+	}{
+		{
+			name: "success",
+			args: args{
+				req: generated.UpdateUserRequest{},
+			},
+			wantStatusCode: http.StatusOK,
+			wantRes: generated.UpdateUserResponse{
+				Success: true,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Server{}
+
+			c = buildHTTPRequestBody(c, method, path, tt.args.req)
+			s.UpdateUser(c)
+
+			var got generated.UpdateUserResponse
+			sonic.Unmarshal(rec.Body.Bytes(), &got)
+
+			if rec.Code != tt.wantStatusCode {
+				t.Errorf("invalid status code, got: %v, want: %v", rec.Code, tt.wantStatusCode)
+			}
+
+			if !reflect.DeepEqual(got, tt.wantRes) {
+				t.Errorf("invalid body response, got: %v, want: %v", got, tt.wantRes)
+			}
+		})
+	}
+}
+
+func TestLoginUser(t *testing.T) {
+	method, path := http.MethodPost, "/users/login"
+	rec, c := initHTTPCall(method, path)
+
+	var (
+		token  = "token-dummy"
+		userID = 1
+	)
+
+	type args struct {
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantStatusCode int
+		wantRes        generated.UserLoginResponse
+	}{
+		{
+			name:           "success",
+			args:           args{},
+			wantStatusCode: http.StatusOK,
+			wantRes: generated.UserLoginResponse{
+				AuthToken: &token,
+				UserId:    &userID,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Server{}
+
+			s.LoginUser(c)
+
+			var got generated.UserLoginResponse
+			sonic.Unmarshal(rec.Body.Bytes(), &got)
+
+			if rec.Code != tt.wantStatusCode {
+				t.Errorf("invalid status code, got: %v, want: %v", rec.Code, tt.wantStatusCode)
+			}
+
+			if !reflect.DeepEqual(got, tt.wantRes) {
+				t.Errorf("invalid body response, got: %v, want: %v", got, tt.wantRes)
+			}
+		})
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	method, path := http.MethodPost, "/users/login"
+	rec, c := initHTTPCall(method, path)
+
+	type args struct {
+	}
+	tests := []struct {
+		name           string
+		args           args
+		wantStatusCode int
+		wantRes        generated.GetUserResponse
+	}{
+		{
+			name:           "success",
+			args:           args{},
+			wantStatusCode: http.StatusOK,
+			wantRes: generated.GetUserResponse{
+				Success: true,
+				Data: &generated.User{
+					FullName:    "Dummy",
+					PhoneNumber: "+62123",
+				},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := Server{}
+
+			s.GetUser(c)
+
+			var got generated.GetUserResponse
+			sonic.Unmarshal(rec.Body.Bytes(), &got)
+
+			if rec.Code != tt.wantStatusCode {
+				t.Errorf("invalid status code, got: %v, want: %v", rec.Code, tt.wantStatusCode)
+			}
+
+			if !reflect.DeepEqual(got, tt.wantRes) {
+				t.Errorf("invalid body response, got: %v, want: %v", got, tt.wantRes)
+			}
+		})
+	}
 }
